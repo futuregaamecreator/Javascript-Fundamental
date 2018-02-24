a8fb48b2b07a5654bfcee1cf530f8246bc0fde97
George Harrison <futuregamecreator@gmail.com>
2018-02-23 21:53:38 -0500

    new updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ///Lecture: boolean logic and switch
-
+/*
 var age = 25;
 if (age < 20){
   .console.log('john is a teenager');
@@ -27,4 +27,88 @@ switch (job) {
 
   default:
 console.log('John does something else');
+}
+*/
+///////////////////////////////////////
+/////Lecture Function
+/*
+function calculateAge(yearofBirth){
+
+  var age = 2017 - yearofBirth;
+  return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1987);
+var ageMary = calculateAge(1965);
+
+
+function yearsUntilRetirement(name, year){
+  var age = calculateAge(year)
+  var retirement = 65 - age;
+
+  if (retirement >= 0){
+
+  console.log(name + ' retires in ' + retirement + ' years');
+}else{
+  console.log(name + ' is already retired');
+}
+}
+
+yearsUntilRetirement('George', 1987);
+yearsUntilRetirement('Mary', 1923);
+*/
+///////////////////////////////////////////////
+////////Lecture: Statements and Expressions
+/*
+//Statements
+function someFun(par) {
+  //code
+}
+
+//Expressions
+var someFun = function(par){
+
+  //code
+}
+
+
+//.Ecpression
+3 + 4;
+var x = 3;
+
+////Statements
+
+if (x == 5 ){
+  //do something
+}
+*/
+
+
+
+/////////////////////////////////
+////Arrays
+
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'talker', false];
+
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+alert(john.indexOf('Smith'));
+
+if ( john.indexOf('teacher') === -1)
+{
+  console.log('John is not a teacher');
 }
