75a7f9c39a68942c59d02aee751d7afb8f19c09c
George Harrison <futuregamecreator@gmail.com>
2018-03-11 22:38:23 -0400

    new

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ if (x == 5 ){
 
 /////////////////////////////////
 ////Arrays
-
+/*
 var names = ['John', 'Jane', 'Mark'];
 var years = new Array(1990, 1969, 1948);
 
@@ -112,3 +112,77 @@ if ( john.indexOf('teacher') === -1)
 {
   console.log('John is not a teacher');
 }
+
+*/
+
+///////////////////////////////////////////
+///Lecture: Objects
+/*
+var john = {
+  name: 'John',
+  lastName:'Smith',
+  yearofBirth: 1990,
+  job: 'teacher',
+  isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName ='Miller';
+john['job'] = 'hacker';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearofBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
+////////////////////////////////////////////////
+///Lecture: Objects and Methonds
+/*
+ver 1.0
+var john = {
+  name: 'John',
+  lastName:'Smith',
+  yearofBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function () {
+    return 2017 - this.yearofBirth;
+  }
+};
+
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+
+john.age = age;
+console.log(john);
+
+*/
+var john = {
+  name: 'John',
+  lastName:'Smith',
+  yearofBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function () {
+    this.age = 2017 - this.yearofBirth;
+  }
+};
+
+
+john.calculateAge();
+console.log(john);
